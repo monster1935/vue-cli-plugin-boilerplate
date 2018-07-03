b2492ee424bed2122023f90be89117e6d0f58371
monster1935 <liyb2014@163.com>
2018-07-03 16:53:13 +0800

    feat: 添加svg的相关loader配置

--- index.js
+++ index.js
@@ -1,24 +1,17 @@
 module.exports = (api, options) => {
-  if (options.svgIcon) {
-    const svgoLoaderConfig = require('svg-sprite-icon/svgo-loader.conf');
-    api.chainWebpack(webpackConfig => {
-      const svgRule = webpackConfig.module
-        .rule('svg')
-          .test(/\.svg$/)
-          .include
-            .add(filepath => {
-              return /src\/assets\/icons/.test(filepath);
-            })
-            .end()
-          .use('svg-sprite-loader')
-            .loader('svg-sprite-loader')
-            .options({
-              symbolId: 'icon-[name]'
-            })
-            .end()
-          .use('svgo-loader')
-            .loader('svgo-loader')
-            .options(svgoLoaderConfig)
-    });
-  }
+  // add svg-icon webpack config
+  const svgoLoaderConfig = require('svg-sprite-icon/svgo-loader.conf');
+  api.chainWebpack(webpackConfig => {
+    const svgRule = webpackConfig.module.rule('svg');
+    svgRule.uses.clear();
+    svgRule.test(/\.svg$/).include.add(path.join(process.cwd(), '.', 'src/assets/icons'));
+    svgRule.use('svg-sprite-loader')
+        .loader('svg-sprite-loader')
+        .options({
+          symbolId: 'icon-[name]'
+        });
+    svgRule.use('svgo-loader')
+        .loader('svgo-loader')
+        .options(svgoLoaderConfig)
+  });
 };
